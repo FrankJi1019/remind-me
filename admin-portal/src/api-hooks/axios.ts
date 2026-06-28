94dc52dce5a4df6ea1d3c5bb0b74d2a0d8849eec
Frank Ji <frankjishiyuan@gmail.com>
2026-06-29 08:34:19 +1200

    chore(admin-portal): move API base URL to environment variable

--- admin-portal/src/api-hooks/axios.ts
+++ admin-portal/src/api-hooks/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'https://9cdd6gmyqb.execute-api.ap-southeast-2.amazonaws.com/prod'
+    baseURL: import.meta.env.VITE_API_BASE_URL
 })
 
 export default apiClient
